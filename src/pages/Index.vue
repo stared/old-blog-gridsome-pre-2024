e4b0e88f173ee1f7d3a1e33fdc860b81055ccc63
Piotr Migdal <pmigdal@gmail.com>
2021-10-10 20:34:35 +0200

    gridsome init blog

---
+++ src/pages/Index.vue
@@ -0,0 +1,69 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, world!</h1>
+
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
+      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
+      Dolores, aperiam non officia eos quod asperiores
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+    </p>
+
+    <h2>Blog posts</h2>
+
+    <div class="post-list">
+      <div v-for="(edge, index) in $page.allBlogPost.edges" :key="index">
+        <p class="title">
+          <g-link :to="edge.node.path" class="read">{{
+            edge.node.title
+          }}</g-link
+          ><br />
+          <span class="description" v-html="edge.node.description" />
+        </p>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<page-query>
+query {
+  allBlogPost {
+    edges {
+      node {
+        id
+        title
+        description
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/templates/BlogPost.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <br />
+    <g-link to="/" class="link"> &larr; Go Back</g-link>
+    <div class="post-title prose max-w-full">
+      <h1>{{ $page.blogPost.title }}</h1>
+      <p class="post-date">
+        {{ $page.blogPost.date }} | {{ $page.blogPost.timeToRead }} min read
+      </p>
+    </div>
+    <div
+      class="post-content prose max-w-full"
+      v-html="$page.blogPost.content"
+    ></div>
+  </Layout>
+</template>
+
+<page-query>
+query BlogPost ($path: String!) {
+   blogPost (path: $path) {
+    id
+    title
+    content
+    date (format: "D MMMM YYYY")
+    timeToRead
+  }
+}
+</page-query>
