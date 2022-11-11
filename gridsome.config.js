013031f5d92712b9eda0e0d8b01abc5bd5519b47
Piotr Migdal <pmigdal@gmail.com>
2022-11-11 10:50:49 +0100

    page name

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Piotr Migdał - Homepage',
   siteUrl: 'https://stared.github.io',
   pathPrefix: '/migdal-blog-gridsome',
   plugins: [
