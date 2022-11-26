244a57f556d9b39e1c32cc66c05a5b81618609d0
Piotr Migdal <pmigdal@gmail.com>
2022-11-26 17:55:39 +0100

    text main page v1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,8 +3,31 @@
     <div class="flex">
       <div class="flex-column text">
         <p>
-          I am excited to look for new, ambitious challenges in projects combining advanced data science with end-user
-          experience.
+          A doctor of sorcery.
+        </p>
+        <p>
+          <b>Profesionally:</b> I am looking for ambitious challenges combining advanced data
+          science with end-user
+          experience. More in <g-link to="/resume">my resume</g-link>.
+        </p>
+        <p>
+          <b>Personally:</b> I dance balfolk and
+          fusion (<i>pretending to dance but actually doing contact improv</i>). I do weekly sauna rituals, among trees,
+          ponds and streams.
+        </p>
+        <p>
+          <b>Here:</b> I invite you to read <g-link to="/blog">my stuff</g-link>, take a look at <g-link to="/projects">
+            my projects</g-link>, skim abstracts of <g-link to="/publications">my academic publications</g-link>.
+        </p>
+        <p>
+          <b>Socially:</b> I love emails, and it is the preferred way to approach me, <code>pmigdal@gmail.com</code>.
+          GitHub is my favorite social network, and here are <a href="https://github.com/stared/">my repos</a>. See also
+          my other accounts:
+          personal
+          <a href="https://www.facebook.com/piotr.migdal.9/">Facebook</a>, semi-used <a
+            href="https://twitter.com/pmigdal">Twitter</a>, professional <a
+            href="https://www.linkedin.com/in/piotrmigdal/">LinkedIn</a>, and link-hoarding <a
+            href="https://pinboard.in/u:pmigdal/">Pinboard</a>.
         </p>
       </div>
       <div class="flex-column image">
